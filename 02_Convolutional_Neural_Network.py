8acdc1e7ec02b44fa1b62ef57309ebea0de363ba
张志豪 <izhangzhihao@hotmail.com>
2017-08-31 23:08:15 +0800

    增加类型

--- 02_Convolutional_Neural_Network.py
+++ 02_Convolutional_Neural_Network.py
@@ -1,51 +1,53 @@
+from typing import Tuple, List
 import tensorflow as tf
 import numpy as np
 import time
 from datetime import timedelta
+
+from tensorflow import Variable
+from tensorflow.contrib.learn.python.learn.datasets.base import Datasets
 from tensorflow.examples.tutorials.mnist import input_data
 
 # Convolutional Layer 1.
-filter_size1 = 5  # Convolution filters are 5 x 5 pixels.
-num_filters1 = 16  # There are 16 of these filters.
+filter_size1: int = 5  # Convolution filters are 5 x 5 pixels.
+num_filters1: int = 16  # There are 16 of these filters.
 
 # Convolutional Layer 2.
-filter_size2 = 5  # Convolution filters are 5 x 5 pixels.
-num_filters2 = 36  # There are 36 of these filters.
+filter_size2: int = 5  # Convolution filters are 5 x 5 pixels.
+num_filters2: int = 36  # There are 36 of these filters.
 
-fc_size = 128  # Number of neurons in fully-connected layer.
+fc_size: int = 128  # Number of neurons in fully-connected layer.
 
-data = input_data.read_data_sets('data/MNIST/', one_hot=True)
+data: Datasets = input_data.read_data_sets('data/MNIST/', one_hot=True)
 
-img_size = 28
-img_size_flat = img_size * img_size
-img_shape = (img_size, img_size)
-num_channels = 1
-num_classes = 10
+img_size: int = 28
+img_size_flat: int = img_size * img_size
+img_shape: Tuple[int, int] = (img_size, img_size)
+num_channels: int = 1
+num_classes: int = 10
 
 
-def new_weights(shape):
+def new_weights(shape: List[int]):
     return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
 
 
-def new_biases(length):
+def new_biases(length: int):
     return tf.Variable(tf.constant(0.05, shape=[length]))
 
 
-def new_conv_layer(input,  # The previous layer.
-                   num_input_channels,  # Num. channels in prev. layer.
-                   filter_size,  # Width and height of each filter.
-                   num_filters,  # Number of filters.
-                   use_pooling=True):  # Use 2x2 max-pooling.
+def new_conv_layer(pre_layer,
+                   num_input_channels: int,
+                   filter_size: int,
+                   num_filters: int,
+                   use_pooling: bool = True):  # Use 2x2 max-pooling.
 
     # Shape of the filter-weights for the convolution.
     # This format is determined by the TensorFlow API.
-    shape = [filter_size, filter_size, num_input_channels, num_filters]
+    shape: List[int] = [filter_size, filter_size, num_input_channels, num_filters]
 
-    # Create new weights aka. filters with the given shape.
-    weights = new_weights(shape=shape)
+    weights: Variable = new_weights(shape)
 
-    # Create new biases, one for each filter.
-    biases = new_biases(length=num_filters)
+    biases: Variable = new_biases(num_filters)
 
     # Create the TensorFlow operation for convolution.
     # Note the strides are set to 1 in all dimensions.
@@ -56,13 +58,10 @@ def new_conv_layer(input,  # The previous layer.
     # is moved 2 pixels across the x- and y-axis of the image.
     # The padding is set to 'SAME' which means the input image
     # is padded with zeroes so the size of the output is the same.
-    layer = tf.nn.conv2d(input=input,
+    layer = tf.nn.conv2d(input=pre_layer,
                          filter=weights,
                          strides=[1, 1, 1, 1],
                          padding='SAME')
-
-    # Add the biases to the results of the convolution.
-    # A bias-value is added to each filter-channel.
     layer += biases
 
     if use_pooling:
@@ -98,14 +97,14 @@ def flatten_layer(layer):
     return layer_flat, num_features
 
 
-def new_fc_layer(input,  # The previous layer.
-                 num_inputs,
-                 num_outputs,
-                 use_relu=True):
-    weights = new_weights(shape=[num_inputs, num_outputs])
-    biases = new_biases(length=num_outputs)
+def new_fc_layer(pre_layer,
+                 num_inputs: int,
+                 num_outputs: int,
+                 use_relu: bool = True):
+    weights: Variable = new_weights([num_inputs, num_outputs])
+    biases: Variable = new_biases(num_outputs)
 
-    layer = tf.matmul(input, weights) + biases
+    layer = tf.matmul(pre_layer, weights) + biases
 
     if use_relu:
         layer = tf.nn.relu(layer)
@@ -118,13 +117,13 @@ x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
 y_true = tf.placeholder(tf.float32, shape=[None, 10], name='y_true')
 y_true_cls = tf.argmax(y_true, axis=1)
 
-layer_conv1 = new_conv_layer(input=x_image,
+layer_conv1 = new_conv_layer(pre_layer=x_image,
                              num_input_channels=num_channels,
                              filter_size=filter_size1,
                              num_filters=num_filters1,
                              use_pooling=True)
 
-layer_conv2 = new_conv_layer(input=layer_conv1,
+layer_conv2 = new_conv_layer(pre_layer=layer_conv1,
                              num_input_channels=num_filters1,
                              filter_size=filter_size2,
                              num_filters=num_filters2,
@@ -132,12 +131,12 @@ layer_conv2 = new_conv_layer(input=layer_conv1,
 
 layer_flat, num_features = flatten_layer(layer_conv2)
 
-layer_fc1 = new_fc_layer(input=layer_flat,
+layer_fc1 = new_fc_layer(pre_layer=layer_flat,
                          num_inputs=num_features,
                          num_outputs=fc_size,
                          use_relu=True)
 
-layer_fc2 = new_fc_layer(input=layer_fc1,
+layer_fc2 = new_fc_layer(pre_layer=layer_fc1,
                          num_inputs=fc_size,
                          num_outputs=num_classes,
                          use_relu=False)
@@ -157,30 +156,30 @@ correct_prediction = tf.equal(y_pred_cls, y_true_cls)
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-train_batch_size = 64
+train_batch_size: int = 64
 
-feed_dict_test = {x: data.test.images,
-                  y_true: data.test.labels,
-                  y_true_cls: np.argmax(data.test.labels, axis=1)}
+feed_dict_test: dict = {x: data.test.images,
+                        y_true: data.test.labels,
+                        y_true_cls: np.argmax(data.test.labels, axis=1)}
 
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
-    total_iterations = 0
+    total_iterations: int = 0
 
 
-    def optimize(num_iterations):
+    def optimize(num_iterations: int):
         # Ensure we update the global variable rather than a local copy.
         global total_iterations
 
-        start_time = time.time()
+        start_time: float = time.time()
 
         for i in range(total_iterations,
                        total_iterations + num_iterations):
 
             x_batch, y_true_batch = data.train.next_batch(train_batch_size)
 
-            feed_dict_train = {x: x_batch,
-                               y_true: y_true_batch}
+            feed_dict_train: dict = {x: x_batch,
+                                     y_true: y_true_batch}
 
             session.run(optimizer, feed_dict=feed_dict_train)
 
@@ -190,16 +189,16 @@ with tf.Session() as session:
                 acc = session.run(accuracy, feed_dict=feed_dict_train)
 
                 # Message for printing.
-                msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
+                msg: str = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
 
                 print(msg.format(i + 1, acc))
 
         # Update the total number of iterations performed.
         total_iterations += num_iterations
 
-        end_time = time.time()
+        end_time: float = time.time()
 
-        time_dif = end_time - start_time
+        time_dif: float = end_time - start_time
 
         print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
