a7d6e5d05cd2b94cda0351f9b21a870c5d1564d3
张志豪 <izhangzhihao@hotmail.com>
2017-10-02 19:58:07 +0800

    Add BasicLSTM

--- BasicLSTM.py
+++ BasicLSTM.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 from tensorflow.contrib.learn.python.learn.datasets.mnist import DataSet
 from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.ops import rnn_cell
 
 mnist: DataSet = input_data.read_data_sets('data/MNIST', one_hot=True)
 
@@ -28,14 +29,15 @@ biases: dict = {
 
 
 def RNN(x, weights: dict, biases: dict):
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, timesteps, n_input)
-    # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
-
-    # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
-    x = tf.unstack(x, timesteps, 1)
     lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
-    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+    lstm_cell = rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=0.5)
+    # 初始化全零 state
+    init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+    # 如果 inputs 为 (batches, steps, inputs) ==> time_major=False
+    # 如果 inputs 为 (inputssteps, batches, inputs) ==> time_major=True
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, initial_state=init_state, time_major=False)
+    # 把 outputs 变成 列表 [(batch, outputs)..] * steps
+    outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
 
@@ -55,7 +57,9 @@ with tf.Session() as session:
     session.run(tf.global_variables_initializer())
 
     for step in range(1, training_steps + 1):
+        # 128 * 784
         batch_x, batch_y = mnist.train.next_batch(batch_size)
+        # 128 * 28 * 28
         batch_x = batch_x.reshape((batch_size, timesteps, num_input))
         session.run(train_op, feed_dict={X: batch_x, Y: batch_y})
         if step % display_step == 0 or step == 1:
@@ -64,8 +68,8 @@ with tf.Session() as session:
             })
             print("Step " + str(step) + "\t loss" + str(loss) + "\t acc" + str(acc))
 
-    test_data = mnist.test.images[:256].reshape((-1, timesteps, num_input))
-    test_label = mnist.test.labels[:256]
+    test_data = mnist.test.images[:128].reshape((-1, timesteps, num_input))
+    test_label = mnist.test.labels[:128]
     print("Test acc " + str(
         session.run(
             accuracy, feed_dict={X: test_data, Y: test_label}
