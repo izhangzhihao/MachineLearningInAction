b928963952dc216af8116ca9f686548365f16dcf
张志豪 <izhangzhihao@hotmail.com>
2017-10-02 16:54:14 +0800

    Add BasicRNN

---
+++ BasicRNN.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from tensorflow.contrib.learn.python.learn.datasets.mnist import DataSet
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.ops import rnn_cell
+
+mnist: DataSet = input_data.read_data_sets('data/MNIST', one_hot=True)
+
+learning_rate: float = 0.001
+training_steps: int = 10000
+batch_size: int = 128
+display_step: int = 200
+
+num_input: int = 28
+timesteps: int = 28
+num_hidden: int = 128
+num_classes: int = 10
+
+X = tf.placeholder(tf.float32, [None, timesteps, num_input])
+Y = tf.placeholder(tf.float32, [None, num_classes])
+
+weight = tf.Variable(tf.random_normal([num_hidden, num_classes]))
+
+bias = tf.Variable(tf.random_normal([num_classes]))
+
+
+def RNN(x, weight, bias):
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, timesteps, n_input)
+    # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
+
+    # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
+    x = tf.unstack(x, timesteps, 1)
+    cell = rnn.BasicRNNCell(num_hidden)
+    cell = rnn_cell.DropoutWrapper(cell, output_keep_prob=0.5)
+    # cell = rnn_cell.MultiRNNCell([cell] * 3)
+    outputs, states = rnn.static_rnn(cell, x, dtype=tf.float32)
+    return tf.matmul(outputs[-1], weight) + bias
+
+
+logits = RNN(X, weight, bias)
+prediction = tf.nn.softmax(logits)
+
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y
+))
+
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_op)
+
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
+
+    for step in range(1, training_steps + 1):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        batch_x = batch_x.reshape((batch_size, timesteps, num_input))
+        session.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+        if step % display_step == 0 or step == 1:
+            loss, acc = session.run([loss_op, accuracy], feed_dict={
+                X: batch_x, Y: batch_y
+            })
+            print("Step " + str(step) + "\t loss" + str(loss) + "\t acc" + str(acc))
+
+    test_data = mnist.test.images[:256].reshape((-1, timesteps, num_input))
+    test_label = mnist.test.labels[:256]
+    print("Test acc " + str(
+        session.run(
+            accuracy, feed_dict={X: test_data, Y: test_label}
+        )
+    ))
