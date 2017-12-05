671a6d2466cedc56a711a8b74767edc650936445
张志豪 <izhangzhihao@hotmail.com>
2017-12-05 17:17:09 +0800

    no message

--- serving/build_ts_serving_source.sh
+++ serving/build_ts_serving_source.sh
@@ -11,7 +11,25 @@ cd /work/serving/tensorflow && ./configure
 # Tensorflow Serving uses Bazel as the build tool. The Docker image already have Bazel installed in it.
 # Run the following command to build the source with Bazel
 
-cd /work/serving && bazel build -c opt //tensorflow_serving/model_servers:tensorflow_model_server --jobs 10 --curses no --discard_analysis_cache
+raw_cpu_flags=`sysctl -a | grep machdep.cpu.features | cut -d ":" -f 2 | tr '[:upper:]' '[:lower:]'`
+COPT="--copt=-march=native"
+
+for cpu_feature in $raw_cpu_flags
+do
+	case "$cpu_feature" in
+		"sse4.1" | "sse4.2" | "ssse3" | "fma" | "cx16" | "popcnt" | "maes")
+		    COPT+=" --copt=-m$cpu_feature"
+		;;
+		"avx1.0")
+		    COPT+=" --copt=-mavx"
+		;;
+		*)
+			# noop
+		;;
+	esac
+done
+
+cd /work/serving && bazel build -c opt ${COPT} //tensorflow_serving/model_servers:tensorflow_model_server --jobs 10 --curses no --discard_analysis_cache
 # The following bazel option flags was added:
 # -c (compilation_mode): the compilation mode flag affects the the C++ generation. ‘opt’ compilation mode is selected to enable optimization and disable the assert calls.
 # — discard_analysis_cache: will discard the analysis cache immediately after the analysis phase completes. This reduces memory usage by ~10%, but makes further incremental builds slower.
